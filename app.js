3cc174c0a5aa0f6cdd0fd082ac3f19dc220df8a3
Malcolm Bird <bird.malcolm@gmail.com>
2014-03-28 13:41:45 +0900

    first iteration

---
+++ app.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+	$("h1").fadeIn(200);
+	$("h2").fadeIn(1500);
+
+//$("body").find("*").addClass("border");
+
+//$("body").delay(5000).queue(function(next){
+//	$("body").find("*").removeClass("border");
+//});
+
+
+$("#headshot").delay(2100).queue(function(next){
+	$(this).fadeIn(100);
+	next();
+});
+
+$("#hello").delay(4000).queue(function(next){
+	$(this).fadeIn(800);
+	next();
+});
+
+
+$("#title").delay(2000).queue(function(next){
+$(this).removeClass("title_initial").addClass("title_afterintro");
+  
+    $("#content").fadeIn(1000);
+    $("#nav_work").addClass("current_tab");
+	$("#work").fadeIn(2000);
+   next();
+});
+
+
+
+$("#nav_work").click(function(event){
+	event.preventDefault();
+	$(".section").hide();
+	$(".current_tab").removeClass("current_tab");
+	$(this).addClass("current_tab");
+	$("#work").slideDown(1200);
+	$("#hello").hide();
+	$("#message").text("These are my latest projects.");
+	$("#hello").fadeIn(1500);
+});
+
+$("#nav_contact").click(function(event){
+	event.preventDefault();
+	$(".section").hide();
+	$(".current_tab").removeClass("current_tab");
+	$(this).addClass("current_tab");
+	$("#contact").slideDown('slow');
+	$("#hello").hide();
+	$("#message").text("I'm just a click away!");
+	$("#hello").fadeIn(1500);
+});
+
+$("#nav_about").click(function(event){
+	event.preventDefault();
+	$(".section").hide();
+	$(".current_tab").removeClass("current_tab");
+	$(this).addClass("current_tab");
+	$("#about").slideDown('slow');
+	$("#hello").hide();
+	$("#message").text("Hire me!");
+	$("#hello").fadeIn(1500);
+});
+
+});
